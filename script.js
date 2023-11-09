252d65a65057da224a66b43d0892409f51bcb240
ali safaa <alisafaaa33@gmail.com>
2023-11-09 22:41:14 +0300

    new update

--- script.js
+++ script.js
@@ -1,47 +1,62 @@
-let emojis = [
-     "🤩",
-     "🤩",
-     "💕",
-     "💕",
-     "🤐",
-     "🤐",
-     "🤣",
-     "🤣",
-     "😎",
-     "😎",
-     "😀",
-     "😀",
-     "😡",
-     "😡",
-     "🤔",
-     "🤔",
-];
+const resetGame = document.querySelector('[data-resetGame]')
+const container = document.querySelector('.container')
+const game = document.querySelector(".game")
 
-let shuf_emojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+let emojis = [
+    '/images/happy-pumpkin.webp',
+    '/images/happy-pumpkin.webp',
+    '/images/halloween-ghost.webp',
+    '/images/halloween-ghost.webp',
+    '/images/halloween.png',
+    '/images/halloween.png',
+    '/images/spider-web.png',
+    '/images/spider-web.png',
+    '/images/spider2.png',
+    '/images/spider2.png',
+    
+];  
+
+ let shuf_emojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
 for (let i = 0; i < emojis.length; i++) {
      let box = document.createElement("div");
+     let img = document.createElement("img");
      box.className = "item";
-     box.innerHTML = shuf_emojis[i];
-     box.onclick = function () {
+     img.src = shuf_emojis[i]
+     img.setAttribute('width', '200px')
+     img.setAttribute('height', '200px')
+     box.appendChild(img)
+     box.addEventListener('click', matchingPhoto)
+
+     function matchingPhoto(){
           this.classList.add("boxOpen");
-          setTimeout(() => {
-               let boxOpen = document.querySelectorAll(".boxOpen");
-               if (boxOpen.length > 1) {
-                    if (boxOpen[0].innerHTML == boxOpen[1].innerHTML) {
-                         boxOpen[0].classList.add("boxMatch");
-                         boxOpen[1].classList.add("boxMatch");
-                         boxOpen[1].classList.remove("boxOpen");
-                         boxOpen[0].classList.remove("boxOpen");
-                         let boxMatch = document.querySelectorAll(".boxMatch");
-                         if (boxMatch.length == emojis.length) {
-                              alert("you win");
-                         }
-                    } else {
-                         boxOpen[1].classList.remove("boxOpen");
-                         boxOpen[0].classList.remove("boxOpen");
+          setTimeout(match, 1000);
+     }
+
+
+     function match(){
+          let boxOpen = document.querySelectorAll(".boxOpen");
+          if (boxOpen.length > 1) {
+               if (boxOpen[0].innerHTML == boxOpen[1].innerHTML) {
+                    boxOpen[0].classList.add("boxMatch");
+                    boxOpen[1].classList.add("boxMatch");
+                    boxOpen[1].classList.remove("boxOpen");
+                    boxOpen[0].classList.remove("boxOpen");
+                    let boxMatch = document.querySelectorAll(".boxMatch");
+                    if (boxMatch.length == emojis.length) {
+                         setTimeout(() => {
+                              container.classList.add('winning')
+                              container.innerHTML = 'You Win'
+                         }, 1000)
                     }
+               } else {
+                    boxOpen[1].classList.remove("boxOpen");
+                    boxOpen[0].classList.remove("boxOpen");
                }
-          }, 1000);
-     };
+          }
+     }
      document.querySelector(".game").appendChild(box);
 }
+
+
+resetGame.addEventListener('click', () => document.location.reload())
